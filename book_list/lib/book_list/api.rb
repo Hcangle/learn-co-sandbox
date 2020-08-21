b83195f0118589943fa60ae22bce9d2f9cc26d28
Hannah Angle <hannahcangle@gmail.com>
2020-08-21 22:04:51 +0000

    Working on finding an API URL that works #2.

--- book_list/lib/book_list/api.rb
+++ book_list/lib/book_list/api.rb
@@ -4,7 +4,7 @@ class API
     #make sure to add a begin rescue incase of errors or nil returns
     key = "BPpJJ9TuVLBucWg9oxqw"
     #url should have genre and api key 53 min into vid
-    url = "https://www.goodreads.com/search.xml?key=#{key}=Ender%27s+Game"
+    url = "https://www.goodreads.com/search.xml?key=#{key}=#{book}"
     response = HTTP.Party.get(url) #error here 56 min on vid 
     #binding.pry 
   end 
