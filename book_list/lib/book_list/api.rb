603675f14325382561fd04c3b8a10122cce33102
Hannah Angle <hannahcangle@gmail.com>
2020-08-23 01:11:58 +0000

    API still isnt working.

--- book_list/lib/book_list/api.rb
+++ book_list/lib/book_list/api.rb
@@ -2,7 +2,7 @@ class API
   
   def self.get_books(book)
     url = "https://www.googleapis.com/books/v1/volumes?q="
-    response = HTTParty.get(url) #error here 56 min on vid 
+    response = HTTParty.get(url) 
     #binding.pry 
   end 
 end 
